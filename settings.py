ddf21b655ef0db2ddd4c66831ed3a36b394c6990
Randall Schwager <schwager@hsph.harvard.edu>
2015-07-01 15:10:05 -0400

    merge

--- settings.py
+++ settings.py
@@ -1,14 +1,15 @@
 ###
 # Config
 
-debug = True
-map_file = "./input/map.txt"
-diet_data = "./input/diet.txt"
+debug = False
+map_file = "./input/hmp2_pilot_data/map.txt"
+diet_data = "./input/hmp2_pilot_data/diet.txt"
 products_dir = "products"
-database_dir = "/home/rschwager/anadama_dev/databases/hmp2vis-leveldb"
+database_dir = "/local/ibdmdb/hmp2-visualizations/databases/hmp2_pilot_data"
 
 class web:
-    host = "0.0.0.0"
-    port = "8081"
-    prefix_url = "" 
+    host = "127.0.0.1"
+    port = "8765"
+    #prefix_url = "/patient" #no prefix by default
+    prefix_url = "/participant" #no prefix by default
     static_url_base = "https://ibdmdb.org/static/"
